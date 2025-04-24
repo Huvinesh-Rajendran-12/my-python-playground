8fa12b6ecbea012e86954640070a7f2c63b5b518
Huvinesh Rajendran <huvinesh@teleme.co>
2025-04-24 16:58:35 +0800

    Remove debug print statement from count_category function in category.py

--- problem_set/category.py
+++ problem_set/category.py
@@ -5,9 +5,6 @@ def count_category(categories: list[tuple[int, str]]) -> int:
             category_counts[category[1]] = 1
         else:
             category_counts[category[1]] += 1
-
-        print(category_counts)
-
     sorted_category_counts = sorted(category_counts.items(), key=lambda x: x[1], reverse=True)
     ans = [item[0] for item in sorted_category_counts][0]
     return ans
